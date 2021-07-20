cfc425dd25ceb590d455be0079617f11a2ef725f
Stephen <stephen.smith@stfc.ac.uk>
2021-07-20 16:40:41 +0100

    Amend mantid-framework meta file

--- recipes/mantid-framework/build.sh
+++ recipes/mantid-framework/build.sh
@@ -8,21 +8,18 @@ cmake \
   ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH=$PREFIX \
-  -GNinja \
   -DCMAKE_FIND_FRAMEWORK=LAST \
+  -DENABLE_WORKBENCH=OFF \
+  -DENABLE_DOCS=OFF \
+  -DUSE_SYSTEM_EIGEN=ON \
   -DENABLE_PRECOMMIT=OFF \
   -DOpenSSL_ROOT=$PREFIX \
   -DHDF5_ROOT=$PREFIX \
   -DPython_ROOT_DIR=$PREFIX \
+  -GNinja \
   ../
 
 ninja
 ninja install
 
-# move mantid
 mv $PREFIX/lib/mantid ${SP_DIR}
-mv $PREFIX/lib/mantid-*-py*.egg-info ${SP_DIR}
-
-# move other workbench related libraries
-mv $PREFIX/lib/mantidqt ${SP_DIR}
-mv $PREFIX/lib/workbench ${SP_DIR}
