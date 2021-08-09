b4f2e8bc950fedcb616f7965df1b33900703072a
Stephen <stephen.smith@stfc.ac.uk>
2021-08-09 09:21:24 +0100

    A couple of changes to get mantidqt working

---
+++ recipes/mantidqt/bld.bat
@@ -0,0 +1,27 @@
+mkdir build && cd build
+
+cmake ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCONDA_BUILD=True ^
+    -DENABLE_WORKBENCH=OFF ^
+    -DENABLE_MANTIDQT=OFF ^
+    -DENABLE_PRECOMMIT=OFF ^
+    -DENABLE_DOCS=OFF ^
+    -DWORKBENCH_SITE_PACKAGES=%SP_DIR% ^
+    -DSTANDALONE_FRAMEWORK=ON ^  
+    -DENABLE_MANTIDQT=ON ^
+    -DUSE_SYSTEM_EIGEN=ON ^
+    ..
+
+if errorlevel 1 exit 1
+cmake --build . --config Release
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+
+@REM set DATA_DIR="%SP_DIR%\mantid"
+@REM if not exist %DATA_DIR% mkdir %DATA_DIR%
+@REM robocopy %LIBRARY_BIN%\mantid %DATA_DIR% * /E
+@REM if %ERRORLEVEL% GTR 3 exit 1
---
+++ recipes/mantidqt/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -ex
+
+mkdir build
+cd build
+
+cmake \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_FIND_FRAMEWORK=LAST \
+  -DENABLE_WORKBENCH=OFF \
+  -DENABLE_DOCS=OFF \
+  -DUSE_SYSTEM_EIGEN=ON \
+  -DWORKBENCH_SITE_PACKAGES=$SP_DIR \
+  -DENABLE_PRECOMMIT=OFF \
+  -DCONDA_BUILD=True \
+  -DOpenSSL_ROOT=$PREFIX \
+  -DHDF5_ROOT=$PREFIX \
+  -DPython_ROOT_DIR=$PREFIX \
+  -DUSE_PYTHON_DYNAMIC_LIB=OFF \
+  -DSTANDALONE_FRAMEWORK=ON \
+  -DENABLE_MANTIDQT=ON \
+  -DQt5_DIR=$PREFIX/lib/cmake/qt5 \
+  -GNinja \
+  ../
+
+ninja
+ninja install
+
+# mv $PREFIX/lib/mantid ${SP_DIR}
